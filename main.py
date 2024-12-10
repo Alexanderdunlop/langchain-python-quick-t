c8af81612ccabcebac70691527ce3346a1705d8a
Alexanderdunlop <alexjamesdunlop@gmail.com>
2024-12-10 18:11:48 +0000

    Enhance main script to accept user input for programming language and generate project ideas using ChatOpenAI

--- main.py
+++ main.py
@@ -3,16 +3,31 @@ import os
 from dotenv import load_dotenv
 
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import (
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate
+)
 
 load_dotenv()
 
 OPENAI_MODEL = "gpt-4"
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
+PROMPT_CODING_PROJECT_INFO = """
+    Provide me with project ideas using {language}.
+    """
+
 def main():
     llm = ChatOpenAI(api_key=OPENAI_API_KEY, model=OPENAI_MODEL)
-    result = llm.predict("Give me 5 project ideas for Python.")
-    print(result)
+
+    language = input("Enter the programming language: ")
+
+    message = HumanMessagePromptTemplate.from_template(template=PROMPT_CODING_PROJECT_INFO)
+    chat_prompt = ChatPromptTemplate.from_messages(messages=[message])
+    chat_prompt_with_values = chat_prompt.format_prompt(language=language)
+
+    response = llm(messages=chat_prompt_with_values.to_messages())
+    print(response)
 
 if __name__ == "__main__":
     main()
